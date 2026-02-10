d733bcf17102984bedc75f5b98751e605616bb0a
charlie <crohitp9@gmail.com>
2026-02-10 12:58:48 +0530

    Update indicator count in MainActivity and upgrade Gradle
    - Set `noOfIndicators` on the progress indicator in `MainActivity` based on the observed `indicatorCount`.
    - Upgrade Gradle from version 7.5 to 8.5 in `gradle-wrapper.properties`.
    - Add `networkTimeout` and `validateDistributionUrl` properties to the Gradle wrapper configuration.

--- app/src/main/java/com/roh/progressindicator/MainActivity.kt
+++ app/src/main/java/com/roh/progressindicator/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             launch(Dispatchers.Main) {
                 viewModel.indicatorCount.collect { count ->
                     binding.txtIndicatorCount.text = count.toString()
-
+                    binding.indicator.noOfIndicators = count
                 }
             }
 
